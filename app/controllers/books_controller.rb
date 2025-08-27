d5b10c463dd1dba4660c411fa656ef6002aaabea
Ubuntu <ubuntu@ip-172-31-12-103.ap-northeast-1.compute.internal>
2025-08-27 14:26:04 +0000

    [bookers2]Remaining numbers 55~57,60~62,64,65

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -19,19 +19,29 @@ class BooksController < ApplicationController
     @book = Book.new(book_params)
     @book.user_id = current_user.id 
     if @book.save
-      redirect_to @book, notice: "Book を作成しました！"
+      redirect_to @book, notice: "You have created book successfully."
     else
       render :new
     end
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
 
   def update
+    @book = Book.find(params[:id])
+  if @book.update(book_params)
+    redirect_to @book, notice: "You have updated book successfully."
+  else
+    render :edit
+  end
   end
 
   def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to '/books'
   end
 
   private
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,8 @@
 class UsersController < ApplicationController
   def index
+    @users = User.all
+    @user = current_user 
+    @book = Book.new
   end
 
   def show
@@ -9,8 +12,20 @@ class UsersController < ApplicationController
   end
 
   def edit
+    @user = User.find(params[:id])
   end
 
   def update
+    @user = User.find(params[:id])
+      if @user.update(user_params)
+        redirect_to user_path(@user), notice: "You have updated user successfully."
+      else
+        render :edit
+      end
   end
+
+  def user_params
+    params.require(:user).permit(:name, :image, :introduction)
+  end
+  
 end
